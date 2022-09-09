ddf57de5f30b301bc7612bb382945e2ce96e7536
elratmacfat <elratmacfat@gmx.de>
2022-09-09 20:25:19 +0200

    automated test runs after successful build

---
+++ make_and_run_tests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#
+# Create build directory
+#
+
+cmake -E make_directory build
+cd build
+
+#
+# Run CMake and abort script on failure
+#
+cmake ..
+
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
+#
+# Run make and abort on failure
+#
+make
+
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
+#
+# Run tests
+#
+
+ctest 
