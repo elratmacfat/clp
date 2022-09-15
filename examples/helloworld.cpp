9bf339482cba81e26d533618072bd3c24bfac021
elratmacfat <elratmacfat@gmx.de>
2022-09-15 14:23:56 +0200

    attempt to improve the readability

--- examples/helloworld.cpp
+++ examples/helloworld.cpp
@@ -1,10 +1,55 @@
+/* This "hello world" example demonstrates the most simple way of setting up and using
+ * the 'Command Line Processor'.
+ */
 #include <elrat/clp.hpp>
 #include <cctype>
 #include <iostream>
 
 using namespace elrat::clp;
 
-void sayHello(const CommandLine& cmdline)
+void print(const std::string&);
+void print(const InputException&);
+
+void sayHelloFunction(const CommandLine&);
+CommandDescriptorPtr createSayHelloDescriptor();
+std::vector<std::string> createInputSequences();
+
+int main()
+{
+    auto sayHelloDescriptor{ createSayHelloDescriptor() };
+    auto inputSequences{ createInputSequences() };
+    
+    Processor processor;                                
+    processor.attach( 
+        sayHelloDescriptor, 
+        sayHelloFunction );
+
+    for(auto& input: inputSequences ) 
+    {
+        print( input );
+        try 
+        {
+            processor.process( input );
+        }
+        catch( InputException& exception ) 
+        {
+            print( exception );
+        }
+    }
+    return 0;
+}
+
+void print(const std::string& input)
+{
+    std::cout << "> " << input << '\n';
+}
+
+void print(const InputException& exception)
+{
+    std::cout << exception.what() << '\n';
+}
+
+void sayHelloFunction(const CommandLine& cmdline)
 {
     static const auto toUpperCase{[](std::string& s){
         static const int offset{32};
@@ -29,7 +74,7 @@ CommandDescriptorPtr createSayHelloDescriptor()
                 "The one you want to greet.",
                 Mandatory,
                 ParameterType::Name, {
-                    Not<std::string>("me", "you")
+                    Not<std::string>("me")
                 })
         }, {
             OptionDescriptor::Create("shout")
@@ -37,42 +82,16 @@ CommandDescriptorPtr createSayHelloDescriptor()
     );
 }
 
-const std::vector<std::string> input_sequences {
-     "sayhello World"   
-    ,"sayhello World --shout"
-    ,"sayhello --shout You" 
-    ,"sayhello not@name"        // @ is an invalid character in 'ParameterType::Name'
-    ,"sayhello"                 // Missing parameter which is declared 'Mandatory'
-    ,"sayhello world --whisper" // Undefined option
-    ,"sayhello me"              // Disallowed parameter value
-    ,"say_hello world"          // Unrecognized command
-};
-
-// Main
-// ----
-// Configure the processor (which is default constructed, and thus deploys the built-in
-// parser), and let it process the input lines defined above.
-// Processing a command line involves the following steps:
-// 1. parsing
-// 2. validating
-// 3. executing
-//
-int main()
+std::vector<std::string> createInputSequences() 
 {
-    Processor processor;                                
-   
-    processor.attach(
-        createSayHelloDescriptor(), 
-        sayHello);
-
-    for(auto& line : input_sequences ) {
-        std::cout << "> " << line << "\n";
-        try {
-            processor.process(line);
-        }
-        catch( InputException& exception ) {
-            std::cout << exception.what() << '\n';
-        }
-    }
-    return 0;
+    return std::vector<std::string>{
+         "sayhello World"   
+	    ,"sayhello World --shout"
+	    ,"sayhello --shout You" 
+	    ,"sayhello not@name"        // @ is an invalid character in 'ParameterType::Name'
+	    ,"sayhello"                 // Missing parameter which is declared 'Mandatory'
+	    ,"sayhello world --whisper" // Undefined option
+	    ,"sayhello me"              // Disallowed parameter value
+	    ,"say_hello world"          // Unrecognized command
+	};
 }
